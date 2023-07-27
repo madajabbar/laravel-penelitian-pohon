da7d2016f2c4d3cd2b3bb6da33fb207c543a1ee2
madajabbar <madajabbar22@gmail.com>
2023-07-27 18:48:25 +0700

    change footer

--- resources/views/backend/layouts/footer.blade.php
+++ resources/views/backend/layouts/footer.blade.php
@@ -1,11 +1,11 @@
 <footer>
     <div class="footer clearfix mb-0 text-muted">
         <div class="float-start">
-            <p>2021 &copy; Mazer</p>
+            <p>2023 &copy; IOTSISKOM</p>
         </div>
         <div class="float-end">
             <p>Crafted with <span class="text-danger"><i class="bi bi-heart"></i></span> by <a
-                    href="https://saugi.me">Saugi</a></p>
+                    href="https://saugi.me">IoTsiskom</a></p>
         </div>
     </div>
 </footer>
